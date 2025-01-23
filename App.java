e8188d885a5bece160290084c91a40f35932eb32
Devin Baysa <baysa@oakland.edu>
2025-01-23 14:46:54 -0500

    Creating a guy that iterates through easy colors

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+
+import org.w3c.dom.events.MouseEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -42,29 +45,59 @@ public class App {
                 simonSaysHard();
             }
         });
-
-        ssPanel.add(easy);
-        ssPanel.add(medium);
-        ssPanel.add(hard);
-        simonSays.add(ssPanel);
-        ssPanel.setVisible(true);
-        simonSays.setVisible(true);
-        simonSays.setSize(300, 200);
-        simonSays.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+        
+    ssPanel.add(easy);
+    ssPanel.add(medium);
+    ssPanel.add(hard);
+    simonSays.add(ssPanel);
+    ssPanel.setVisible(true);
+    simonSays.setVisible(true);
+    simonSays.setSize(300, 200);
+    simonSays.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 
     public void simonSaysEasy() {
         System.out.println("Easy mode selected");
-        
+        String[] easyColors = {"Red", "Blue", "Yellow", "Green"};
+
+        JFrame easyFrame = new JFrame("Easy Mode");
+        easyFrame.setSize(300, 200);
+        easyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JLabel easyLabel = new JLabel("Easy Mode", JLabel.CENTER);
+        easyFrame.getContentPane().add(easyLabel);
+        Timer timer = new Timer(2000, new ActionListener() {
+            int index = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                easyLabel.setText(easyColors[index]);
+                index = (index + 1) % easyColors.length;
+            }
+        });
+        timer.start();
+
+        easyFrame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                timer.stop();
+            }
+        });
+
+        easyFrame.setVisible(true);
+
     }
 
     public void simonSaysMedium() {
         System.out.println("Medium mode selected");
+        String[] mediumColors = {"Red", "Blue", "Yellow", "Green", "Purple", "Orange"};
+        
         // Add your logic for Medium mode here
     }
 
     public void simonSaysHard() {
         System.out.println("Hard mode selected");
+        String[] hardColors = {"Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Black", "Pink"};
         // Add your logic for Hard mode here
     }
 
