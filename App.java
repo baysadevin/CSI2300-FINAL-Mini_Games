08b983ebda11adb0feacd14a180a209563bee8be
Devin Baysa <baysa@oakland.edu>
2025-01-26 20:20:09 -0500

    added color switcher

--- App.java
+++ App.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Color;
+import java.util.Random;
 
 public class App {
 
@@ -58,11 +60,12 @@ public class App {
 
     public void simonSaysEasy() {
         System.out.println("Easy mode selected");
-        String[] easyColors = {"Red", "Blue", "Yellow", "Green"};
+        String[] easyColors = {"RED", "BLUE", "YELLOW", "GREEN"};
 
         JFrame easyFrame = new JFrame("Easy Mode");
         easyFrame.setSize(300, 200);
         easyFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        
 
         JLabel easyLabel = new JLabel("Easy Mode", JLabel.CENTER);
         easyFrame.getContentPane().add(easyLabel);
@@ -71,8 +74,9 @@ public class App {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                easyLabel.setText(easyColors[index]);
-                index = (index + 1) % easyColors.length;
+                String thisColor = easyColors[new Random().nextInt(easyColors.length)];
+                easyLabel.setText(thisColor);
+                easyFrame.getContentPane().setBackground( thisColor == "RED" ? Color.RED : thisColor == "BLUE" ? Color.BLUE : thisColor == "YELLOW" ? Color.YELLOW : Color.GREEN); 
             }
         });
         timer.start();
@@ -90,15 +94,66 @@ public class App {
 
     public void simonSaysMedium() {
         System.out.println("Medium mode selected");
-        String[] mediumColors = {"Red", "Blue", "Yellow", "Green", "Purple", "Orange"};
+        String[] mediumColors = {"RED", "BLUE", "YELLOW", "GREEN", "PURPLE", "ORANGE"};
         
-        // Add your logic for Medium mode here
+        JFrame mediumFrame = new JFrame("Medium Mode");
+        mediumFrame.setSize(300, 200);
+        mediumFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JLabel mediumLabel = new JLabel("medium Mode", JLabel.CENTER);
+        mediumFrame.getContentPane().add(mediumLabel);
+        Timer timer = new Timer(2000, new ActionListener() {
+            int index = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String thisColor = mediumColors[new Random().nextInt(mediumColors.length)];
+                mediumLabel.setText(thisColor);
+                mediumFrame.getContentPane().setBackground( thisColor == "RED" ? Color.RED : thisColor == "BLUE" ? Color.BLUE : thisColor == "YELLOW" ? Color.YELLOW : thisColor == "GREEN" ? Color.GREEN : thisColor == "PURPLE" ? Color.MAGENTA : Color.ORANGE);
+            }
+        });
+        timer.start();
+
+        mediumFrame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                timer.stop();
+            }
+        });
+
+        mediumFrame.setVisible(true);
+
     }
 
     public void simonSaysHard() {
         System.out.println("Hard mode selected");
         String[] hardColors = {"Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Black", "Pink"};
-        // Add your logic for Hard mode here
+        JFrame hardFrame = new JFrame("Hard Mode");
+        hardFrame.setSize(300, 200);
+        hardFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JLabel hardLabel = new JLabel("Hard Mode", JLabel.CENTER);
+        hardFrame.getContentPane().add(hardLabel);
+        Timer timer = new Timer(2000, new ActionListener() {
+            int index = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String thisColor = hardColors[new Random().nextInt(hardColors.length)];
+                hardLabel.setText(thisColor);
+                hardFrame.getContentPane().setBackground( thisColor == "Red" ? Color.RED : thisColor == "Blue" ? Color.BLUE : thisColor == "Yellow" ? Color.YELLOW : thisColor == "Green" ? Color.GREEN : thisColor == "Purple" ? Color.MAGENTA : thisColor == "Orange" ? Color.ORANGE : thisColor == "Black" ? Color.BLACK : Color.PINK);
+            }
+        });
+        timer.start();
+
+        hardFrame.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                timer.stop();
+            }
+        });
+
+        hardFrame.setVisible(true);
     }
 
     public static void main(String[] args) {
@@ -106,29 +161,4 @@ public class App {
         app.gui();
     }
 }
-    //public static void main(String[] args){
-    //    App app = new App();
-      //  app.gui();
-        //List easyColors = new ArrayList();
-      //  List mediumColors = new ArrayList();
-      //  List hardColors = new ArrayList();
-      //  easyColors.add("Red");
-      //  easyColors.add("Blue");
-      //  easyColors.add("Yellow");
-       // easyColors.add("Green");
-       // mediumColors.add("Red");
-      //  mediumColors.add("Blue");
-      //  mediumColors.add("Yellow");
-      //  mediumColors.add("Green");
-      //  mediumColors.add("Purple");
-      //  mediumColors.add("Orange");
-      //  hardColors.add("Red");
-      //  hardColors.add("Blue");
-      //  hardColors.add("Yellow");
-      //  hardColors.add("Green");
-      //  hardColors.add("Purple");
-      //  hardColors.add("Orange");
-      //  hardColors.add("Black");
-      //  hardColors.add("Pink");
-  //  }
 
